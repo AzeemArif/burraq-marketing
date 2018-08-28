021a84509321cb176db63b9860443c272b114d4f
Azeem Arif <mwaqas.shah@gmail.com>
2018-08-28 18:21:54 +0500

    thumbnails added

--- burraqMarketing/settings.py
+++ burraqMarketing/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "burraq",
+    "imagekit",
 ]
 
 MIDDLEWARE = [
@@ -132,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
